98c70b52251c3f3af6dacf2cefd5b86c8c2111b3
Rutia08 <rutia08@gmail.com>
2022-10-02 19:21:51 +0300

    базовая стилизация мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 let page = document.getElementsByTagName('body') [0];
 let navSite = document.querySelector('.site-nav');
 let navToggle = document.querySelector('.site-nav__toggle');
+let allInputs = document.querySelectorAll('.form__input');
 let mapPicture = document.querySelector('.map__picture');
 
 page.classList.remove('nojs');
@@ -19,10 +20,18 @@ var swiper = new Swiper(".mySwiper", {
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
   mousewheel: true,
   keyboard: true,
 });
+
+// disabled для label
+for (let i = 0; i < allInputs.length; i++) {
+  if (allInputs[i].disabled) {
+    allInputs[i].parentNode.classList.add('form__label--disabled');
+  }
+}
 
 // карта
 mapPicture.classList.add('visually-hidden');
